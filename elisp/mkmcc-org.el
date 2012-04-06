f335ee45856a371854d8feab4f80659411409d5b
Mike McCourt <mkmcc@astro.berkeley.edu>
2012-04-05 22:47:32 -0700

    fixed validation links in org-export

--- elisp/mkmcc-org.el
+++ elisp/mkmcc-org.el
@@ -61,13 +61,11 @@
 
 ;; add validator links with images
 (setq org-export-html-validation-link
-      "<p class=\"xhtml-validation\">
+      "<p class=\"validation\">
      <a href=\"http://validator.w3.org/check?uri=referer\">
        <img src=\"http://w3.org/Icons/valid-xhtml10\"
             alt=\"Valid XHTML 1.0 Strict\" height=\"31\" width=\"88\" />
      </a>
-   </p>
-   <p class=\"css-validation\">
      <a href=\"http://jigsaw.w3.org/css-validator/check?uri=referer\">
        <img style=\"border:0; width:88px; height:31px\"
             src=\"http://jigsaw.w3.org/css-validator/images/vcss-blue\"
