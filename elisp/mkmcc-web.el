b8df9eba1ca64b7a1947f20e267715d695a891f9
Mike McCourt <mkmcc@astro.berkeley.edu>
2012-07-27 15:21:12 -0700

    ads elvis

--- elisp/mkmcc-web.el
+++ elisp/mkmcc-web.el
@@ -4,7 +4,10 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; define my own elvi
-
+(nero-defelvis "Nasa ADS"
+  "http://adsabs.harvard.edu/cgi-bin/nph-basic_connect?qsearch="
+  "%20"
+  "&version=1")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; fin
 (provide 'mkmcc-web)
