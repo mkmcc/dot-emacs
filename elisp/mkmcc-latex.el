0587c358732e4ca160a025799b858f48586abdc9
Mike McCourt <mkmcc@astro.berkeley.edu>
2012-09-25 17:41:23 -0700

    add functions to locate non-ascii characters
    I don't really have a file for general text/editing things, so I've
    stuck this in the latex file for now.  It would be a good idea to
    clean things up later...

--- elisp/mkmcc-latex.el
+++ elisp/mkmcc-latex.el
@@ -37,7 +37,32 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; functions to locate non-ascii characters
+(defun find-first-non-ascii-char ()
+  "Find the first non-ascii character from point onwards."
+  (interactive)
+  (let (point)
+    (save-excursion
+      (setq point
+            (catch 'non-ascii
+              (while (not (eobp))
+                (or (eq (char-charset (following-char))
+                        'ascii)
+                    (throw 'non-ascii (point)))
+                (forward-char 1)))))
+    (if point
+        (goto-char point)
+      (message "No non-ascii characters."))))
 
+(defun occur-non-ascii ()
+  "Find any non-ascii characters in the current buffer."
+  (interactive)
+  (occur "[^[:ascii:]]"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; make rubber the default method for compiling.  bind f9 to compile
 ;;; and f12 to view.
 (defun mkmcc-latex-mode-hook ()
@@ -57,6 +82,7 @@
   (setq TeX-command-default '"Rubber"))
 
 (add-hook 'LaTeX-mode-hook 'mkmcc-latex-mode-hook)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (provide 'mkmcc-latex)
