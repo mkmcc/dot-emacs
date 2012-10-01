bdf18ecef9546a7992fb201a5f48ac243f102b24
Mike McCourt <mkmcc@astro.berkeley.edu>
2012-10-01 11:56:10 -0700

    added unfill-paragraph and unfill-region to mkmcc-latex.el
    latex.el is really turning into a general text mode -- I really should
    split it!

--- elisp/mkmcc-latex.el
+++ elisp/mkmcc-latex.el
@@ -63,6 +63,25 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; unfill paragraph and unfill region
+(defun unfill-paragraph ()
+  "Replace newline chars in current paragraph by single spaces.
+This command does the inverse of `fill-paragraph'."
+  (interactive)
+  (let ((fill-column most-positive-fixnum))
+    (fill-paragraph nil)))
+
+(defun unfill-region (start end)
+  "Replace newline chars in region from START to END by single spaces.
+This command does the inverse of `fill-region'."
+  (interactive "r")
+  (let ((fill-column most-positive-fixnum))
+    (fill-region start end)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; make rubber the default method for compiling.  bind f9 to compile
 ;;; and f12 to view.
 (defun mkmcc-latex-mode-hook ()
