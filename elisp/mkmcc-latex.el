9fce3da195ada470e7f5cdc716a03ca12436107f
Mike McCourt <mkmcc@astro.berkeley.edu>
2012-10-22 23:43:00 -0700

    removed find-first-non-ascii.  occur-non-ascii is much better

--- elisp/mkmcc-latex.el
+++ elisp/mkmcc-latex.el
@@ -37,23 +37,7 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; functions to locate non-ascii characters
-(defun find-first-non-ascii-char ()
-  "Find the first non-ascii character from point onwards."
-  (interactive)
-  (let (point)
-    (save-excursion
-      (setq point
-            (catch 'non-ascii
-              (while (not (eobp))
-                (or (eq (char-charset (following-char))
-                        'ascii)
-                    (throw 'non-ascii (point)))
-                (forward-char 1)))))
-    (if point
-        (goto-char point)
-      (message "No non-ascii characters."))))
-
+;; locate non-ascii characters
 (defun occur-non-ascii ()
   "Find any non-ascii characters in the current buffer."
   (interactive)
