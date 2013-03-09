723c2f83abd8edea286cda0c8d8824716633fb79
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-03-08 21:54:45 -0800

    dired

--- elisp/mkmcc-dired.el
+++ elisp/mkmcc-dired.el
@@ -18,6 +18,21 @@
     (ediff3 (buffer-file-name (nth 0 marked-files))
             (buffer-file-name (nth 1 marked-files))
             (buffer-file-name (nth 2 marked-files)))))
+
+; remap 'o' in dired mode to open a file
+(defun dired-open-mac ()
+  (interactive)
+  (let ((file-name (dired-get-file-for-visit)))
+    (if (file-exists-p file-name)
+        (call-process "/usr/bin/open" nil 0 nil file-name))))
+
+(eval-after-load "dired"
+  '(progn
+     (define-key dired-mode-map "o" 'dired-open-mac)
+     (define-key dired-mode-map "-" 'dired-up-directory)))
+
+; prefer dired over dumping dir list to buffer
+(global-set-key "\C-x\C-d" 'dired)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'mkmcc-dired)
