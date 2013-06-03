395446710c4902c88dfc623a21ab2bcf5f91a0bd
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-06-03 11:41:12 -0700

    make M-< and M-> work with dired

--- elisp/mkmcc-dired.el
+++ elisp/mkmcc-dired.el
@@ -13,6 +13,16 @@
             (buffer-file-name (nth 1 marked-files))
             (buffer-file-name (nth 2 marked-files)))))
 
+(defun dired-back-to-top ()
+  (interactive)
+  (beginning-of-buffer)
+  (dired-next-line 4))
+
+(defun dired-jump-to-bottom ()
+  (interactive)
+  (end-of-buffer)
+  (dired-next-line -1))
+
 ; remap 'o' in dired mode to open a file
 (defun dired-open-mac ()
   (interactive)
@@ -24,6 +34,15 @@
   '(progn
      (define-key dired-mode-map "o" 'dired-open-mac)
      (define-key dired-mode-map "-" 'dired-up-directory)
+
+     (define-key dired-mode-map
+       (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
+     (define-key dired-mode-map
+       (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
+
+     (setq dired-recursive-deletes 'always
+           dired-recursive-copies 'always
+           dired-dwim-target t)
 
      (defun dired-multi-occur (string)
        "Search string in files marked by dired."
