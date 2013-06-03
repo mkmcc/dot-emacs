06e358c0840aaff889841e76fe2d189301d33dee
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-06-03 11:58:11 -0700

    macro to rename major modes in modeline

--- elisp/mkmcc-ui.el
+++ elisp/mkmcc-ui.el
@@ -30,6 +30,13 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; modeline
+(defmacro rename-modeline (package-name mode new-name)
+  `(eval-after-load ,package-name
+     '(defadvice ,mode (after rename-modeline activate)
+        (setq mode-name ,new-name))))
+
+(rename-modeline "clojure-mode" clojure-mode "Clj")
+
 (setq-default
  mode-line-format
  '(;; Position, including warning for 80 columns
