2767090837c4ce3024dbbd3167c88605b48f4b81
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-06-17 12:05:24 -0700

    make paredit happy with delete-selection-mode

--- elisp/prelude-lisp.el
+++ elisp/prelude-lisp.el
@@ -49,6 +49,15 @@
 
 (eval-after-load "paredit"
   '(progn
+
+     ;; making paredit work with delete-selection-mode
+     (put 'paredit-forward-delete 'delete-selection 'supersede)
+     (put 'paredit-backward-delete 'delete-selection 'supersede)
+     (put 'paredit-open-round 'delete-selection t)
+     (put 'paredit-open-square 'delete-selection t)
+     (put 'paredit-doublequote 'delete-selection t)
+     (put 'paredit-newline 'delete-selection t)
+
      (defun paredit-wrap-round-from-behind ()
        (interactive)
        (forward-sexp -1)
