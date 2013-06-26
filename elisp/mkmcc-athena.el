4290bef551a6ab6b4d4a85274457769b97ddd45a
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-06-25 18:27:18 -0700

    refinements

--- elisp/mkmcc-athena.el
+++ elisp/mkmcc-athena.el
@@ -8,9 +8,7 @@
   (flyspell-prog-mode)
   (prelude-local-comment-auto-fill)
   (prelude-turn-on-whitespace)
-  ;(subword-mode +1)
   (prelude-add-watchwords)
-  ;; keep the whitespace decent all the time
   (add-hook 'before-save-hook 'whitespace-cleanup nil t))
 
 (add-hook 'athinput-mode-hook 'prelude-prog-mode-hook)
