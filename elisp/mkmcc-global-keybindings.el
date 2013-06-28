d0f14e0ad38ec0d29ae4c1f315cddc47345316b1
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-06-28 16:57:08 -0700

    shortcut for typing λ

--- elisp/mkmcc-global-keybindings.el
+++ elisp/mkmcc-global-keybindings.el
@@ -2,12 +2,17 @@
 ;; global keybindings
 ;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; keybindings expect interactive commands.  define a shorthand for
 ;; interactive lambdas
 (defmacro λ (&rest body)
   `(lambda ()
      (interactive)
      ,@body))
+
+(global-set-key (kbd "s-l") (λ (insert "\u03bb")))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; fix the keybindings on osx
