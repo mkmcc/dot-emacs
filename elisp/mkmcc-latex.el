bede10deac6162cc53e730ad6ea1d62560d6dde2
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-06-28 10:51:24 -0700

    forget auctex -- I prefer yasnippet to everything it does.  use reftex for clever autocompletion and some simple elisp functions to fill in the rest.

--- elisp/mkmcc-latex.el
+++ elisp/mkmcc-latex.el
@@ -1,46 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; latex mode tweaks
 ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; AUCTeX configuration
-(defvar TeX-auto-save)
-(defvar TeX-parse-self)
-(defvar TeX-PDF-mode)
-(defvar TeX-view-program-selection)
-(defvar TeX-view-program-list)
-
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-
-(setq-default TeX-master nil)
-
-;; use pdflatex
-(setq TeX-PDF-mode t)
-
-(setq TeX-view-program-selection
-      '((output-dvi  "DVI Viewer")
-        (output-pdf  "PDF Viewer")
-        (output-html "HTML Viewer")))
-
-;; this section is good for OS X only
-(setq TeX-view-program-list
-      '(("DVI Viewer"  "open %o")
-        ("PDF Viewer"  "open %o")
-        ("HTML Viewer" "open %o")))
-
-;; add rubber as an option in the compile menu
-(eval-after-load "tex"
-  '(progn
-     (add-to-list 'TeX-expand-list
-                  '("%(RubberPDF)"
-                    (lambda ()
-                      (if (not TeX-PDF-mode)
-                          ""
-                        "--pdf"))))
-     (add-to-list 'TeX-command-list
-                  '("Rubber" "rubber %(RubberPDF) %t" TeX-run-shell nil t) t)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; locate non-ascii characters
@@ -50,30 +10,34 @@
   (occur "[^[:ascii:]]"))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defun mkmcc-set-latex-compile-command ()
+  "create a buffer-local compile command for latex buffers."
+  (when buffer-file-name
+    (let ((cmd "rubber -df --warn all"))
+      (setq-local compile-command
+                  (concat cmd " " buffer-file-name)))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; make rubber the default method for compiling.  bind f9 to compile
-;;; and f12 to view.
-(defvar TeX-mode-map)
-(defvar TeX-command-default)
+(defun mkmcc-view-latex-file ()
+  "find the pdf associated with a latex file and open it."
+  (interactive)
+  (when buffer-file-name
+    (shell-command
+     (concat "open "
+             (file-name-sans-extension buffer-file-name)
+             ".pdf"))))
 
 (defun mkmcc-latex-mode-hook ()
-  (define-key TeX-mode-map (kbd "<f9>")
-    (lambda ()
-      (interactive)
-      (save-buffer)
-      (TeX-command-menu "Rubber")
-      (TeX-clean)))
-  (define-key TeX-mode-map (kbd "<f12>")
-    (lambda ()
-      (interactive)
-      (TeX-view)
-      [return]))
-  (setq TeX-command-default '"Rubber"))
+  "defaults for `latex-mode'."
+  (turn-on-reftex)
+  (diminish 'reftex-mode)
+  (diminish 'abbrev-mode "…")
+  (turn-on-auto-fill)
 
+  (mkmcc-set-latex-compile-command)
+  (local-set-key (kbd "<f8>") 'mkmcc-view-latex-file))
+
+(add-hook 'latex-mode-hook 'mkmcc-latex-mode-hook)
 (add-hook 'LaTeX-mode-hook 'mkmcc-latex-mode-hook)
-(add-hook 'LaTeX-mode-hook 'abbrev-mode)
-(add-hook 'LaTeX-mode-hook 'turn-on-auto-fill)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
