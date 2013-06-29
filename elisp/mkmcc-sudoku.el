4b90dbf550c7b3be2a18c561a56364d1af491444
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-06-29 00:34:38 -0700

    remove sudoku.  haven't used it in years

--- elisp/mkmcc-sudoku.el
+++
@@ -1,7 +0,0 @@
-(require 'sudoku)
-
-(setq sudoku-download-method "native-url-lib")
-(setq sudoku-download nil)
-(setq sudoku-level "hard")              ; easy|hard|medium|evil
-
-(provide 'mkmcc-sudoku)
