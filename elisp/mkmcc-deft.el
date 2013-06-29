991f273288f475c86610f1d777d2362565ef87e3
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-06-29 00:39:13 -0700

    BUGFIX: the variable deft-strip-title-regex changed to deft-strip-title-regexp in the new version of deft.  My customization was being ignored

--- elisp/mkmcc-deft.el
+++ elisp/mkmcc-deft.el
@@ -7,7 +7,7 @@
 (setq deft-directory "~/.emacs.d/deft/") ; keep my ~ clean
                                          ; note trailing / is impt.
 (global-set-key [f5] 'deft)
-(setq deft-strip-title-regex "^#\\+\\w+:[ \t]+\\|^[#* ]*")
+(setq deft-strip-title-regexp "^#\\+\\w+:[ \t]+\\|^[#* ]*")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'mkmcc-deft)
