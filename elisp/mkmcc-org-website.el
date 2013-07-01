51aec147482f785effc856757acc4ae38c1ee02e
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-06-30 18:16:31 -0700

    whitespace

--- elisp/mkmcc-org-website.el
+++ elisp/mkmcc-org-website.el
@@ -19,16 +19,16 @@
 (defvar org-export-html-validation-link)
 (setq org-export-html-validation-link
       "<p class=\"validation\">
-     <a href=\"http://validator.w3.org/check?uri=referer\">
-       <img src=\"http://w3.org/Icons/valid-xhtml10\"
-            alt=\"Valid XHTML 1.0 Strict\" height=\"31\" width=\"88\" />
-     </a>
-     <a href=\"http://jigsaw.w3.org/css-validator/check?uri=referer\">
-       <img style=\"border:0; width:88px; height:31px\"
-            src=\"http://jigsaw.w3.org/css-validator/images/vcss-blue\"
-            alt=\"Valid CSS\" />
-     </a>
-   </p>")
+  <a href=\"http://validator.w3.org/check?uri=referer\">
+     <img src=\"http://w3.org/Icons/valid-xhtml10\"
+          alt=\"Valid XHTML 1.0 Strict\" height=\"31\" width=\"88\" />
+  </a>
+  <a href=\"http://jigsaw.w3.org/css-validator/check?uri=referer\">
+     <img style=\"border:0; width:88px; height:31px\"
+          src=\"http://jigsaw.w3.org/css-validator/images/vcss-blue\"
+          alt=\"Valid CSS\" />
+  </a>
+</p>")
 
 ;; use my own css
 (defvar org-export-html-style-include-default)
