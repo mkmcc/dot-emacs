e798ea7d4bf114408ae7b15204313f22a207eb11
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-07-09 16:41:33 -0700

    added a function to copy a bibtex definition from ADS

--- elisp/mkmcc-web.el
+++ elisp/mkmcc-web.el
@@ -8,6 +8,20 @@
   "http://adsabs.harvard.edu/cgi-bin/nph-basic_connect?qsearch="
   "%20"
   "&version=1")
+
+(defun nero-ads-copy-bibtex ()
+  "Extract the bibtex definition from a nero buffer
+and save it to the kill ring."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (re-search-forward "\\(@\\w+\\)")
+    (let ((front (match-string-no-properties 1))
+          (bpoint (point)))
+      (forward-sexp 1)
+      (kill-new (concat
+                 front
+                 (buffer-substring-no-properties bpoint (point)))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'mkmcc-web)
