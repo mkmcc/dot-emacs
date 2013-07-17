35dfaa717f09c41352af4319e25a917399af5dd5
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-07-17 14:50:54 -0700

    remove more autoload statements

--- elisp/mkmcc-gnuplot.el
+++ elisp/mkmcc-gnuplot.el
@@ -1,8 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; gnuplot scripts
 ;;
-(autoload 'gnuplot-mode "gnuplot-mode.el")
-(add-auto-mode 'gnuplot-mode "\\.gp\\'" "\\.gnuplot\\'")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'mkmcc-gnuplot)
