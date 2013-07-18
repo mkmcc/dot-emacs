d40c4791bf8052bdc070317e1adb7bfbf14f02fb
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-07-18 01:11:01 -0700

    added a discussion of autoloading
    this took me a while to figure out, so i figured i should write it down
    somewhere.

--- vendor/update-auto-loads.el
+++ vendor/update-auto-loads.el
@@ -1,4 +1,37 @@
-(require 'autoload)         ;ironic, i know
+;; update-auto-loads.el --- process autoload cookies in the vendor dir
+;;
+;; Autoloading makes function names available to emacs, without having
+;; to load the entire file which defines them.  When you call the
+;; function, emacs loads the file to retrieve the function definition.
+;; So rather than loading everything at startup, you can load things
+;; piecemeal as you use them.  neat!
+;;
+;; An autoload statement has the form (autoload 'smex "smex"), where
+;; the first argument is the function name and the second is the name
+;; of the file which defines it (without the .el extension; including
+;; the .el extension will cause it to ignore byte-compiled files!).
+;;
+;; Compared to `require', autoload statements are a pain to maintain
+;; manually -- you need to list every function you might want.
+;; Fortunately, emacs can create them automatically.  You tell emacs
+;; which functions to autoload by prefacing their definitions with the
+;; 'magic' comment ;;;###autoload.  Emacs generates the autoload
+;; statements and saves them to a file called `loaddefs.el'.  You can
+;; then load that in your init file.
+;;
+;; If autoload doesn't recognize the statement as a defun, it simply
+;; copies the whole expression into loaddefs.el.  You can use this for
+;; quick things like adding to auto-mode-alist, etc.
+;;
+;; Emacs already processes autoload statements for built-in things and
+;; for things installed via package.el.  This file adds functionality
+;; for things you installed manually.  Specifically, it assumes you
+;; have put code in the folder stored in `vendor-dir', and it
+;; processes every .el file in that dir and in the first level of
+;; subdirectories.  The output is a loaddefs.el inside vendor-dir,
+;; which you can load explicitly at the end of your init.
+;;
+(require 'autoload)
 (require 'dash)
 (defvar vendor-dir)
 
@@ -13,7 +46,7 @@
 
 ;;;###autoload
 (defun update-vendor-autoloads (dir)
-  "Update autoloads for files in the diretory containing this file."
+  "Process autoloads in DIR and first-level subdirectories."
   (interactive)
   (let ((generated-autoload-file (expand-file-name "loaddefs.el" dir)))
     (when (not (file-exists-p generated-autoload-file))
@@ -32,3 +65,5 @@
             (message "Updating vendor autoloads...")
             (update-vendor-autoloads vendor-dir)
             (message "Updating vendor autoloads... done.")))
+
+;;; update-auto-loads.el ends here
