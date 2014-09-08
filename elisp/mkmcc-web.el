05c0d2250035c876728face8aee28fe10e96b1b4
Mike McCourt <mkmcc@astro.berkeley.edu>
2014-09-08 15:33:36 -0400

    hackernews face

--- elisp/mkmcc-web.el
+++ elisp/mkmcc-web.el
@@ -1,3 +1,12 @@
 (require 'nero)
 
+(defface hackernews-link-face
+  '((t (:foreground "green")))
+  "Face used for links to articles"
+  :group 'hackernews)
+
+(set-face-attribute 'hackernews-link-face nil
+                    :foreground nil
+                    :inherit 'font-lock-string-face)
+
 (provide 'mkmcc-web)
